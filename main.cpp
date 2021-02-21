3c1da80741d997565e9dccdc6f87c0ddfff5a940
Lili <charlyrsmail@gmail.com>
2021-02-21 22:06:39 +0300

    added main.cpp
    it does not contain any valuable code but I added it anyway

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_set>
+#include <set>
+#include "List.h"
+
+
+int main() {
+    Program program;
+    program.Run();
+    return 0;
+}
