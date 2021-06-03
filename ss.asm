852d9ec73c8db3375a715ac17ff49c826f0e9753
Liliya <charlyrsmail@gmail.com>
2021-06-03 11:11:51 +0300

    +

--- ss.asm
+++ ss.asm
@@ -5,40 +5,40 @@
 
 
 _Polyndrom proc
-								mov al, 0
-								mov edi, dword ptr[esp+4]
+	mov al, 0
+	mov edi, dword ptr[esp+4]
 	
-								mov ecx, -1
+	mov ecx, -1
 _continue:
-								add ecx, 1
-								scasb
-								jne _continue;
+	add ecx, 1
+	scasb
+	jne _continue;
 
 
-								mov edi, dword ptr[esp+4]
-								mov esi, dword ptr[esp+4]
-								add esi, ecx
-								add esi, -1
+	mov edi, dword ptr[esp+4]
+	mov esi, dword ptr[esp+4]
+	add esi, ecx
+	add esi, -1
 
 _check:
-								cmp ecx, 0
-								jng _true;
-								add ecx, -2
-								mov eax, 0
-								std
-								lodsb
-								cld
-								scasb
-								je _check;
+	cmp ecx, 0
+	jng _true;
+	add ecx, -2
+	mov eax, 0
+	std
+	lodsb
+	cld
+	scasb
+	je _check;
 
-								mov eax, 0
-								jmp _end;
+	mov eax, 0
+	jmp _end;
 
 _true:
-								mov eax, 1
+	mov eax, 1
 
 _end:
 
-		ret 
+	ret 
 _Polyndrom endp
 end
